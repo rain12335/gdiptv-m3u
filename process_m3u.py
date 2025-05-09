a3fb541ccf3c683e94c0403f4a7728bb40c691df
golne999 <81290021+golne999@users.noreply.github.com>
2025-05-09 18:54:52 +0800

    Update process_m3u.py

--- process_m3u.py
+++ process_m3u.py
@@ -1,16 +1,26 @@
 import requests
 
-url = "https://raw.githubusercontent.com/Tzwcard/ChinaTelecom-GuangdongIPTV-RTP-List/refs/heads/master/GuangdongIPTV_rtp_hd.m3u"
+urls = [
+    "https://raw.githubusercontent.com/Tzwcard/ChinaTelecom-GuangdongIPTV-RTP-List/refs/heads/master/GuangdongIPTV_rtp_hd.m3u",
+    "https://raw.githubusercontent.com/Tzwcard/ChinaTelecom-GuangdongIPTV-RTP-List/refs/heads/master/GuangdongIPTV_rtp_4k.m3u"
+]
+
 replace_prefix = "http://192.168.9.133:7088/udp/"
+merged_content = ""
+
+for url in urls:
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        content = response.text
+        new_content = content.replace("rtp://", replace_prefix)
+        merged_content += new_content.strip() + "\n"
+    except Exception as e:
+        print(f"下载失败：{url}\n错误：{e}")
 
 try:
-    response = requests.get(url, timeout=10)
-    response.raise_for_status()
-    content = response.text
-    new_content = content.replace("rtp://", replace_prefix)
-
     with open("GuangdongIPTV_http.m3u", "w", encoding="utf-8") as f:
-        f.write(new_content)
-    print("M3U 文件处理成功。")
+        f.write(merged_content)
+    print("合并并处理完成。")
 except Exception as e:
-    print("下载或处理出错：", str(e))
+    print("保存文件失败：", str(e))
